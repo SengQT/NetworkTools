8d3dfa86576313cb5bc3c1f5eb22d877e86fc81d
SengQT <kyexd16@gmail.com>
2026-04-03 14:10:52 +0700

    initial commit: port scanner

---
+++ NetworkTools/scanner.py
@@ -0,0 +1,14 @@
+import socket
+
+class PortScanner:
+    def __init__(self,target,thread=100):
+        self.target = target
+        self.thread = thread
+        self.open_ports = []
+    
+    def scan_port(self,port):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(0.5)
+        if s.connect_ex((self.target, port)) == 0:
+            self.open_ports.append(port)
+        s.close()
